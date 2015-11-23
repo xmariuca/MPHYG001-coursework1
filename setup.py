bd08090963ddf13edc09231007db9e4e2e1be3b0
Mariuca Xm <mariarrobu@gmail.com>
2015-11-23 17:38:29 +0000

    added setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-
+from setuptools import setup, find_packages
+setup(
+    name = "Greengraph",
+    version = "0.1",
+    packages = find_packages(exclude=['*test']),
+    scripts = ['scripts/greengraph'],
+    install_requires = ['argparse']
+)
