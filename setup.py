b11493d80e4d423888c8185cd61ac065604e1aef
Maria Robu <ucabmrr@ucl.ac.uk>
2016-01-06 10:32:28 +0000

    added more libraries to install_requires

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     version = "0.1",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/getGreenGraph'],
-    install_requires = ['argparse']
+    install_requires = [
+        'argparse',
+        'matplotlib',
+        'geopy',
+        'numpy'
+        ]
 )
